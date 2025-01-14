dfc134da731d7aea50ad0ac379f490da5a2d1d5c
packituz <pacohm94@gmail.com>
2025-01-14 15:12:15 -0600

    changed message

--- app.py
+++ app.py
@@ -1,11 +1,30 @@
+import os.path
+
 from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 async def root():
-    return {"message": "Hello World 44"}
+    message_file: str = 'message.txt'
+    message = ''
+    if os.path.exists(message_file):
+        with open(message_file) as file:
+            for line in file:
+                message = f'{message} {line}'
+    else:
+        message = 'default'
+
+    return {"message": f"Hello World {message}"}
+
 
 @app.get("/posts")
 async def get_posts():
     return [{"id": 1, "title": "First Post", "content": "This is the content of the first post."}]
+
+
+if __name__ == '__main__':
+    import uvicorn
+
+    uvicorn.run(app, host='0.0.0.0', port=8081)
